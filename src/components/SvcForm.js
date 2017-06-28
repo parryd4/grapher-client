7164e068c6388d8fb93f607bafbe739d0e280963
Dan Parry <parryd424@gmail.com>
2017-06-28 07:20:07 -0400

    promise enables passing file content

--- src/components/SvcForm.js
+++ src/components/SvcForm.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react'
 import ReactFileReader from 'react-file-reader'
 import * as d3 from 'd3'
+import TableDisplay from './TableDisplay'
+
 export default class SvcForm extends Component {
 
   constructor() {
     super()
+    // trying to use a variable??
+    let pleaseWork
     this.state = {
 
     }
     this.handleFiles = this.handleFiles.bind(this)
+    this.prepareData = this.prepareData.bind(this)
+  }
+
+  prepareData(data) {
+//    console.log(data)
+    let ssv = d3.dsvFormat(";")
+    let cleanData = ssv.parse(data)
+    this.setState({
+      data: cleanData
+    })
   }
 
   handleFiles(files) {
     // this.setState({ filename: files[0].name.split('.')[0] })
+
+/* original, no promise
     let reader = new FileReader()
+
     reader.onload = function(e) {
+      console.log(reader)
 
       let allData = reader.result
-      let rows = reader.result.split('\n')
+      // let rows = reader.result.split('\n')
       // rows.forEach(e => console.log(e) )
-      let headings = rows[0].split(';')
 
+      // let headings = rows[0].split(';')
 
+      this.pleaseWork = allData
+      console.log(this.pleaseWork)
       // https://github.com/d3/d3-dsv
       let ssv = d3.dsvFormat(";")
       let data = ssv.parse(allData)
@@ -31,7 +51,7 @@ export default class SvcForm extends Component {
 
       let space = document.getElementsByClassName("test")[0]
       let tbody = document.getElementById("dataHolder")
-      console.log(tbody)
+
       // headings.forEach(e => space.innerHTML += (`<th>${e}</th>`))
       data.columns.forEach(e => space.innerHTML += (`<th>${e}</th>`))
 
@@ -40,22 +60,44 @@ export default class SvcForm extends Component {
         let insertRows = data.columns.map(c => (`<td>${d[c]}</td>`) )
         tbody.innerHTML += (`<tr> ${insertRows.join(" ")} </tr>`)
       })
-
-
     }
 
-    reader.readAsText(files[0])
-    this.setState({
-      filename: files[0].name.split('.')[0]
-    })
+   reader.readAsText(files[0])
+*/
+
+    // using Promise to use file info outside of onload
+    if (files[0]) {
+      new Promise(function(resolve, reject) {
+        let reader = new FileReader()
+        reader.onload = function(evt) {
+          resolve(evt.target.result)
+        }
+        reader.readAsText(files[0])
+        reader.onerror = reject
+      })
+      .then(this.prepareData)
+      .catch(function(err) {
+        console.log(err)
+      })
+    }
+
+    // this.setState({
+    //   filename: files[0].name.split('.')[0]
+    // })
+
+    console.log( files[0].name.split('.')[0] )
   }
 
   render() {
     return (
       <div>
+        {this.state.filename ? <TableDisplay tableData={this.prepareData(this.pleaseWork)}/> : <p>Hi Mom</p>}
+        {/*this.state.filename ? <TableDisplay tableData={"something passes through"}/> : <p>Hi Mom</p>*/}
+        {this.state.data ? console.log(this.state.data.length) : console.log("no data loaded")}
         <ReactFileReader handleFiles={this.handleFiles} fileTypes={'.csv'}>
-          <button className='btn'>'Select A File To Upload'</button>
+          <button className='btn'>{this.state.filename || 'Select A File To Upload'}</button>
         </ReactFileReader>
+
         <div>
           <table>
             <tbody id="dataHolder">
--- src/components/TableDisplay.js
+++ src/components/TableDisplay.js
@@ -1,5 +1,44 @@
 import React, { Component } from 'react'
+import * as d3 from 'd3'
 
 export default class TableDisplay extends Component {
+  constructor(){
+    super()
+
+  }
+
+  componentDidMount() {
+    // both these things work with "something passes through" and renderreturning this.state.filename
+    console.log('mounted')
+    console.log(this.props.tableData)
+
+  }
+  /*
+  let space = document.getElementsByClassName("test")[0]
+  let tbody = document.getElementById("dataHolder")
+
+  // headings.forEach(e => space.innerHTML += (`<th>${e}</th>`))
+  data.columns.forEach(e => space.innerHTML += (`<th>${e}</th>`))
+
+  data.forEach(function(d){
+
+    let insertRows = data.columns.map(c => (`<td>${d[c]}</td>`) )
+    tbody.innerHTML += (`<tr> ${insertRows.join(" ")} </tr>`)
+  })
+
+  */
+  render() {
+    return (
+      <div>
+      <p>{this.props.tableData}</p>
+        <table>
+          <tbody id="dataGoesHere">
+            <tr id="headings">
+            </tr>
+            </tbody>
+        </table>
+      </div>
+    )
+  }
 
 }
